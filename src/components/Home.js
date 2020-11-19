6a1aa448ea2589ddabfb9153f0d649a8f1919b99
bebusl <brill_be@naver.com>
2020-11-19 11:24:59 +0900

    Home화면 수정

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,10 @@ import UserService from "../services/user.service";
 const Home = () => {
   const [content, setContent] = useState("");
 
-
   return (
     <div className="container">
       <header className="jumbotron">
-        <h3>SW중심사업단 ?시스템</h3>
+        <h3>SW중심대학사업단 Keep-UpⓇ 관리 시스템</h3>
       </header>
     </div>
   );

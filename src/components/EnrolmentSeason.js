7a45b1f7f4af92c93474b900ac84394e712679d3
bebusl <brill_be@naver.com>
2020-12-29 10:22:03 +0900

    기간 변경 - 기간 뜨도록 수정

--- src/components/EnrolmentSeason.js
+++ src/components/EnrolmentSeason.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { toaster } from "evergreen-ui";
 
 const EnrolmentSeason = (props) => {
@@ -10,10 +10,45 @@ const EnrolmentSeason = (props) => {
     semester: 1,
   });
 
+  const [setTime_start, settingStartTime] = useState(0);
+  const [setTime_end, settingEndTime] = useState(0);
+
+  useEffect(() => {
+    Axios.get(`/api/systems/find/1/${values.year}/${values.semester}`)
+      .then((res) => {
+        if (res.data.result !== null) {
+          settingEndTime(res.data.result.end);
+          settingStartTime(res.data.result.start);
+        }
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  useEffect(() => {
+    Axios.get(`/api/systems/find/1/${values.year}/${values.semester}`)
+      .then((res) => {
+        if (
+          res.data.result !== null &&
+          res.data.result.start !== setTime_start &&
+          res.data.result.end !== setTime_end
+        ) {
+          settingEndTime(res.data.result.end);
+          settingStartTime(res.data.result.start);
+          console.log("될까?", setTime_start, res.data.result.start);
+        }
+      })
+      .catch((error) => console.log(error));
+  }, [values.year, values.semester]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     console.log(name, value);
     setValues({ ...values, [name]: value });
+  };
+  const toDate = (timestamp) => {
+    const newDate = new Date(timestamp);
+    return `${newDate.getFullYear()}년 ${
+      newDate.getMonth() + 1
+    }월 ${newDate.getDate()}일`;
   };
 
   return (
@@ -21,17 +56,19 @@ const EnrolmentSeason = (props) => {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          console.log(new Date(values.start).getTime());
-          console.log(new Date(values.end).getTime());
+          const start = new Date(values.start).getTime();
+          const end = new Date(values.end).getTime();
           Axios.post("/api/systems/modify", {
             systemId: 1,
             year: values.year,
             semester: values.semester,
-            start: new Date(values.start).getTime(),
-            end: new Date(values.end).getTime(),
+            start: start,
+            end: end,
           }).then((res) => {
             toaster.success("기간 변경 성공");
             console.log("제출해봄", res);
+            settingStartTime(start);
+            settingEndTime(end);
           });
         }}
       >
@@ -55,6 +92,9 @@ const EnrolmentSeason = (props) => {
           </div>
         </div>
         <h5>수강신청 기간 설정</h5>
+        <p>
+          현재기간 : {toDate(setTime_start)} ~ {toDate(setTime_end)}
+        </p>
         <input
           type="date"
           name="start"

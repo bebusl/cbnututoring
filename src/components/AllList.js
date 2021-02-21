d5e1f824ffd6fa3d97bb989f50169aa7bcd857a7
bebusl <bebus1998@naver.com>
2021-02-21 10:18:24 +0900

    fix: 로그인 권한 뺏기-전체리스트 페이지에서 누락된 것 수정

--- src/components/AllList.js
+++ src/components/AllList.js
@@ -43,7 +43,7 @@ const AllList = ({ years, isCourseManage = false ,history}) => {
       .get(`/api/courses/find/${year}/${semester}`)
       .then(function (response) {
         console.log("검색결과", response);
-        if(response.data.success ===false){
+        if(response.data.success ===false && response.data.msg === "인증 실패!"){
           setLoginStatus(false);
           setUserData({});
           toaster.danger("다른 컴퓨터에서 로그인이 되어서 종료됩니다.");

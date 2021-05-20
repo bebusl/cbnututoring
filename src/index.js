339e2acbb49d2c6b30e8889f9cdf739321c53d3f
bebusl <bebus1998@naver.com>
2021-05-21 00:17:48 +0900

    sharing with macc

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { Provider } from "react-redux";
 import store from "./store/store";
-
+import "./index.css";
 import App from "./App";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Noto Sans CJK KR"',
+  },
+});
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
     </Provider>
   </BrowserRouter>,
   document.getElementById("root")

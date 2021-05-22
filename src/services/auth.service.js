03447b930faaeaf2851e2a668cb12eedaad4cdb0
bebusl <bebus1998@naver.com>
2021-05-23 00:34:37 +0900

    [추가]axios interceptor설정으로 중복 로그인 방지 로직 구현

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,1 +1,10 @@
 import axios from "./withAxios";
+//import axios from "axios";
+const login = (_id, password) => {
+  return axios.post("/api/accounts/login", {
+    _id,
+    password,
+  });
+};
+
+export default login;
--- src/services/withAxios.js
+++ src/services/withAxios.js
@@ -1,13 +1,30 @@
 import axios from "axios";
+const AUTH_FALSE = "AUTH_FALSE";
+const axios_ = axios.create();
+//axios사용해서 오류시 return AuthFailed
 
-export default function axios_(req) {
-  return req.then((res) => {
-    if (res.data.success === false && res.data.msg === "인증 실패!") {
-      // setLoginStatus(false);
-      // setUserData({});
-      // toaster.danger("다른 컴퓨터에서 로그인이 되어서 종료됩니다.");
-      // props.history.push("/login");
-      //에러페이지 따로 만들어서 거기서 처리하게 하긩~~!^0^
-    }
-  });
-}
+axios_.interceptors.response.use(
+  function (response) {
+    if (response.data.success === false && response.data.msg === "인증 실패!") {
+      return AUTH_FALSE;
+    } else return response;
+  },
+  function (error) {
+    console.error("ERROR", error);
+    return Promise.reject(error);
+  }
+);
+
+// export default function axios_(req) {
+//   return req.then((res) => {
+//     if (res.data.success === false && res.data.msg === "인증 실패!") {
+//       // setLoginStatus(false);
+//       // setUserData({});
+//       // toaster.danger("다른 컴퓨터에서 로그인이 되어서 종료됩니다.");
+//       // props.history.push("/login");
+//       //에러페이지 따로 만들어서 거기서 처리하게 하긩~~!^0^
+//     }
+//   });
+// }
+
+export default axios_;
